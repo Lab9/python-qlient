d5d21043daba8a327c368bc780378205385630ae
Daniel Seifert <info@danielseifert.ch>
2020-09-24 16:07:15 +0200

    add missing import

--- qlient/__init__.py
+++ qlient/__init__.py
@@ -1,8 +1,8 @@
 from .builder import fields
 from .client import Client
-from .proxy import QueryServiceProxy, MutationServiceProxy
+from .proxy import QueryServiceProxy, MutationServiceProxy, SubscriptionServiceProxy
 from .schema import Schema
 from .settings import Settings
 from .transport import Transporter, AsyncTransporter
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
